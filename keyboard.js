585ce72d7698b9c04931c5ae10f176ccaf9202b8
chelsea barocio <barocio.chelsea@gmail.com>
2021-02-18 14:39:38 -0800

    backspace logic added

--- keyboard.js
+++ keyboard.js
@@ -34,9 +34,50 @@ const Keyboard = {
 
     _createKeys() {
         const fragment = document.createDocumentFragment();
-        const keyLayout = [];
+        const keyLayout = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
 
-    },
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+
+            "space"
+        ];
+
+        //create icon html
+
+        const createIconHTML = (icon_name) => {
+            return '<i class="material-icons"><$icon_name</i>'
+        };
+
+        keyLayout.forEach(key => {
+            const keyElement = document.createElement("button");
+            const insertLineBreak = ["backpspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+
+            //adding attribbutes and classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard_key");
+
+            switch (key) {
+                case "backspace":
+                    keyElement.classList.add("keyboard_key--wide");
+                    keyElement.innerHTML = createIconHTML("backspace");
+
+                    //backspace delete event listener
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+            }
+
+        });
+
+        },
 
     triggerEvents(handlerName) {
         console.log("Event Triggered!" + handlerName);
