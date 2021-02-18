213300e758c42e1687c2b27a30c591b43f49027e
chelsea barocio <barocio.chelsea@gmail.com>
2021-02-18 14:15:31 -0800

    adding JS

---
+++ keyboard.js
@@ -0,0 +1,61 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys:[],
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false;
+    },
+
+
+    init() {
+        //create the new elements here
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+
+        //setup keys 
+        this.elements.main.classList.add("keyboard", "3keyboard-hidden");
+        this.elements.keysContainer.classList.add("keyboard_keys");
+
+
+        //add elements to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+    },
+
+    _createKeys() {
+
+    },
+
+    triggerEvents(handlerName) {
+        console.log("Event Triggered!" + handlerName);
+
+    },
+
+    toggleCapsLock() {
+        console.log("Caps Lock Toggled!");
+    },
+
+    open(initialValue, oninput, onclose) {
+
+    },
+
+    close() {
+
+    },
+  
+};
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    Keyboard.init();
+});
